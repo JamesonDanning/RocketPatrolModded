1dd81d18735175ee9e53e7e0b32f8a6d5602ae70
Jameson Danning <jdanning@ucsc.edu>
2020-04-14 14:46:45 -0700

    assets added, fish swim

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,38 +6,63 @@ class Play extends Phaser.Scene {
 
     preload() {
 //load images and tile sprite
-        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('bobber', './assets/bobber.png');
         this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('river', './assets/river.png');
+        this.load.image('dirt', './assets/dirt.png');
         this.load.spritesheet('explosion', './assets/explosion.png', 
             {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('fish', './assets/fish.png', 
+            {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 4});
     }
 
     create() {
         //place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        this.river = this.add.tileSprite(0, 0, 640, 480, 'river').setOrigin(0, 0);
+        this.dirt = this.add.sprite(0, 0, 'dirt').setOrigin(0, 0);
 
-        this.add.rectangle(5,5,630,32, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5,443,630,32, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5,5,32,455, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(603,5,32,455, 0xFFFFFF).setOrigin(0,0);
+        //this.add.rectangle(5,5,630,32, 0xFFFFFF).setOrigin(0,0);
+        //this.add.rectangle(5,443,630,32, 0xFFFFFF).setOrigin(0,0);
+        //this.add.rectangle(5,5,32,455, 0xFFFFFF).setOrigin(0,0);
+        //this.add.rectangle(603,5,32,455, 0xFFFFFF).setOrigin(0,0);
         //green ui background
-        this.add.rectangle(37, 42, 566, 64, 0x00ff00).setOrigin(0, 0);
+        //this.add.rectangle(37, 42, 566, 64, 0x00ff00).setOrigin(0, 0);
 
-        //add rocket
-        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 
-            'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
+        this.anims.create({
+            key: 'bob',
+            frames: this.anims.generateFrameNumbers('bobber', {start: 0, end: 1, first: 0}, true),
+            frameRate: 4,
+            repeat: -1
+        });
 
+        //add bobber
+        this.p1Bobber = new Rocket(this, game.config.width/2, 431, 
+            'bobber').setOrigin(0, 0);
+        //this.p1Bobber.anims.play('bob');
 
-            //add spaceship x3
-            this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship',
+            //fish swimming anim
+        this.anims.create({
+            key: 'swim',
+            frames: this.anims.generateFrameNumbers('fish', {start: 0, end: 4, first: 0}, true),
+            frameRate: 8,
+            repeat: -1
+        });
+       
+
+            //add fish x3
+            this.fish01 = new Spaceship(this, game.config.width + 192, 132, 'fish',
                  0, 30).setOrigin(0,0);
+                 this.fish01.anims.play('swim');
 
-             this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship',
-                 0, 20).setOrigin(0,0);
+             this.fish02 = new Spaceship(this, game.config.width + 96, 196, 'fish',
+                 0, 20).setScale(0.75, 0.75).setOrigin(0,0);
+                 this.fish02.anims.play('swim');
 
-            this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship',
-                 0, 10).setOrigin(0,0);
+            this.fish03 = new Spaceship(this, game.config.width, 260, 'fish',
+                 0, 10).setScale(0.5, 0.5).setOrigin(0,0);
+                 this.fish03.anims.play('swim');
+
+            
 
             // define keyboard keys
             keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -48,7 +73,10 @@ class Play extends Phaser.Scene {
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
             frameRate: 30
+            
         });
+
+        
 
         //score
         this.p1Score = 0; 
@@ -79,6 +107,9 @@ class Play extends Phaser.Scene {
             this.gameOver = true;
         }, null, this);
 
+        //add fish swimming
+        //let fish1 = this.add.sprite(300, 200, 'fish').setOrigin(0, 0);
+        //fish1.anims.play('swim');
     }
 
     update() {
@@ -90,36 +121,38 @@ class Play extends Phaser.Scene {
             this.scene.start("menuScene");
         }
         
-        //scroll starfield
-        this.starfield.tilePositionX -= 4;
+        //scroll river
+        this.river.tilePositionX += 1;
 
         if(!this.gameOver){
-            this.p1Rocket.update();
-            this.ship01.update();
-            this.ship02.update();
-            this.ship03.update();
+            this.p1Bobber.update();
+            this.fish01.update();
+            this.fish02.update();
+            this.fish03.update();
         }
 
-        if(this.checkCollision(this.p1Rocket, this.ship03)) {
-            this.p1Rocket.reset();
-            this.shipExplode(this.ship03);   
+        if(this.checkCollision(this.p1Bobber, this.fish03)) {
+            this.p1Bobber.reset();
+            this.shipExplode(this.fish03);   
         }
-        if (this.checkCollision(this.p1Rocket, this.ship02)) {
-            this.p1Rocket.reset();
-            this.shipExplode(this.ship02);
+        if (this.checkCollision(this.p1Bobber, this.fish02)) {
+            this.p1Bobber.reset();
+            this.shipExplode(this.fish02);
         }
-        if (this.checkCollision(this.p1Rocket, this.ship01)) {
-            this.p1Rocket.reset();
-            this.shipExplode(this.ship01);
+        if (this.checkCollision(this.p1Bobber, this.fish01)) {
+            this.p1Bobber.reset();
+            this.shipExplode(this.fish01);
         }
         
     }
 
-    checkCollision(rocket, ship) {
-        if (rocket.x < ship.x + ship.width &&
-            rocket.x + rocket.width > ship.x &&
-            rocket.y < ship.y + ship.height &&
-            rocket.height + rocket.y > ship.y) {
+    
+
+    checkCollision(bobber, ship) {
+        if (bobber.x < ship.x + ship.width &&
+            bobber.x + bobber.width > ship.x &&
+            bobber.y < ship.y + ship.height &&
+            bobber.height + bobber.y > ship.y) {
                 return true;
         } else {
             return false
